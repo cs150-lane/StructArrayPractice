7fcbb808bcf5c6870fc7345e22e0887584376ca7
Chris Lane <lanec@pacificu.edu>
2022-11-29 19:59:28 -0800

    Initial Push with TODOs

---
+++ StructArrayPractice/main.cpp
@@ -0,0 +1,151 @@
+//*********************************************************************
+// File name:			main.cpp
+// Author:				CS Faculty
+// Date:					November 30, 2022
+// Class:					CS 150-0X
+// Assignment:		Struct and Array In Class Activity
+// Purpose:				More practice with structs and arrays of structs
+// Hours Worked:	50 minutes
+//*********************************************************************
+
+#include <iostream>
+using namespace std;
+
+// TODO #1: Copy the struct Weather definition into main.cpp
+
+
+// TODO #4a: Write the printWeather function prototype
+
+
+// TODO #5a: Write the printAllWeather function prototype
+
+
+// TODO #8a: Write the hottestDay function prototype
+
+
+// TODO #10a: Write the biggestDay function prototype
+
+
+// TODO 14a (BONUS): Write the writeWeather function protype
+
+
+// TODO 15a (BONUS): Write the readWeather function protype
+
+
+
+int main (){
+	// TODO #2: Delcare an array of 31 Weather structs in main ()
+
+
+	// TODO #3: Fill in first 7 array positions with the given data
+
+
+	// TODO #7: In main, print the weather in array positions 0, 1, 2. 
+
+
+	// TODO #9: In main, print the weather on the hottest day
+
+
+	// TODO #11: In main, print the weather on the hottest day
+
+
+	// TODO #12: In main, add the weather {30.0, 20.0, 0.0. 14.0} in 
+	//					 position 0 of the array and shift the existing items 
+	//					 in the array one slot to make room
+
+
+	// TODO #13: In main, print the weather in all the array positions
+
+
+
+
+	return EXIT_SUCCESS;
+}
+
+// TODO #4b: Write the function printWeather which will print one 
+//					 Weather structure
+
+//*********************************************************************
+// Function:		printWeather
+//
+// Description: 
+// 
+// Parameters:	
+//
+// Returned:		none
+//*********************************************************************
+
+
+
+// TODO #5b: Write the printAllWeather function which will print every
+//					 Weather struct in an array
+
+//*********************************************************************
+// Function:		printAllWeather
+//
+// Description: 
+// 
+// Parameters:	
+//
+// Returned:		none
+//*********************************************************************
+
+
+
+// TODO #8b: Write the hottestDay function which will find the day with
+//           the highest high temperature and return the index of that 
+//           day in the array
+
+//*********************************************************************
+// Function:		hottestDay
+//
+// Description: 
+// 
+// Parameters:	
+//
+// Returned:		
+//*********************************************************************
+
+
+
+// TODO #10b: Write the biggestChange function which will find the day
+//						with the biggest difference between the high and low
+//						temperature and return the index of the day in the array.
+
+//*********************************************************************
+// Function:		biggestChange
+//
+// Description: 
+// 
+// Parameters:	
+//
+// Returned:		
+//*********************************************************************
+
+
+
+
+// TODO #14b: Write the writeWeather function definition
+
+//*********************************************************************
+// Function:		writeWeather
+//
+// Description: 
+// 
+// Parameters:	
+//
+// Returned:		
+//*********************************************************************
+
+
+// TODO #15b: Write the readWeather function definition
+
+//*********************************************************************
+// Function:		readWeather
+//
+// Description: 
+// 
+// Parameters:	
+//
+// Returned:		
+//*********************************************************************
